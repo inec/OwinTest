a4cda0b7ce2c9c4dba38b37a672f08837536d169
YChi Lu <luyege+github@gmail.com>
2015-06-29 12:02:02 -0500

    Temp

---
+++ Demo.Security/DemoPrinciple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Security
+{
+    public class DemoPrincipal : ClaimsPrincipal
+    {
+
+
+        public DemoPrincipal(DemoIdentity identity)
+            : base(identity)
+        {
+
+        }
+
+        public DemoPrincipal(ClaimsPrincipal claimsPrincipal)
+            : base(claimsPrincipal)
+        {
+
+        }
+    }
+}
